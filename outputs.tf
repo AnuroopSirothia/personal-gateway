84aebf266b9d7ec2bb11fe0448210802d5859c24
SIROTHIA <anuroop.sirothia@ing.com>
2024-12-30 18:01:04 +1100

    Updated output to see public IP address
    Now it is easier to find the public IP address and RDP to the VM.

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+output "public_ip_address" {
+  description = "Public IP Address of the Virtual Machine"
+  value = azurerm_windows_virtual_machine.vm.public_ip_address
 }
